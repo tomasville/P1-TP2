8d644528dfdb6d840bf706ffd4bcb82a7aba9576
Pablo Aguilar <paedmaag@gmail.com>
2025-11-12 21:07:08 -0300

    Se implementaron los metodos de ListaEnlazada en app.java
    Se crearon una nueva lista y un nuevo nodo para ponerlos a prueba.

--- App/src/App.java
+++ App/src/App.java
@@ -1,5 +1,18 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        ListaEnlazada l1 = new ListaEnlazada();
+        l1.agregarAlInicio(3);
+        l1.agregarAlInicio(2);
+        l1.agregarAlInicio(1);
+        l1.recorrer();
+        
+        ListaEnlazada l2;
+        Nodo nodito = new Nodo(10);
+        l2 = new ListaEnlazada(nodito);
+        l2.agregarAlInicio(20);
+        l2.recorrer(); 
+    
+        
     }
 }
